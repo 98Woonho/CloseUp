c1d02397a1e563f76309fbcb401ea6de09d5de8c
98Woonho <9woonho8@gmail.com>
2024-07-01 23:30:08 +0900

    .

---
+++ src/main/java/com/example/closeup/CloseUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.closeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloseUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/closeup/CloseUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.closeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloseUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

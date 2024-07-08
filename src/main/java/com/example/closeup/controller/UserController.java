d7e4fcc9e3cd822e8bbe31bc188c190637617476
98Woonho <9woonho8@gmail.com>
2024-07-08 18:48:00 +0900

    login.js 작업중

---
+++ src/main/java/com/example/closeup/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.closeup.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("login")
+    public void getLogin() {
+
+    }
+}
---
+++ src/main/resources/static/js/user/login.js
@@ -0,0 +1,56 @@
+/* input 입력값 정규식 */
+// 아이디 정규 표현식 공식(대/소문자영어, 숫자, 공백X, 5~15자 이내)
+const CK_id = /^[a-zA-Z][0-9]{5,15}$/;
+// 공백 확인 정규 표현식
+const CK_space = /\s/g;
+// 비밀번호 정규 표현식 공식
+const CK_pw = /^[A-Za-z\\d`~!@#$%^&*()-_=+]{8,20}$/;
+
+
+/*
+    JS
+    1. 아이디, 비밀번호 정규식 확인
+    (오류시 html의 .error p 문구 display="block";)
+    (오류시, 잘못입력한 input에 focus시키고 작성한 input값 = ''으로 내용 비우기)
+
+    2. 아이디 저장 checked하면 그 value값 저장
+
+*/
+
+const loginForm = document.getElementById('loginForm');
+const loginBtn = document.querySelector('.login-btn');
+const errMsgs = document.querySelectorAll('.err-msg');
+
+// 로그인 버튼 클릭
+loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    errMsgs.forEach(errMsg => {
+        errMsg.style.display = 'none';
+    })
+
+    if (loginForm['id'].value === '') {
+        errMsgs[1].style.display = 'block';
+        return;
+    }
+
+    if (!RegExp(loginForm['id'].value).test(CK_space)) {
+        errMsgs[0].style.display = 'block';
+        return;
+    }
+
+    if (!RegExp(loginForm['id'].value).test(CK_id)) {
+        errMsgs[1].style.display = 'block';
+        return;
+    }
+
+    if (!RegExp(loginForm['pw'].value).test(CK_space) || loginForm['pw'].value === '') {
+        errMsgs[0].style.display = 'block';
+        return;
+    }
+
+    if (!RegExp(loginForm['pw'].value).test(CK_pw)) {
+        errMsgs[2].style.display = 'block';
+        return;
+    }
+})

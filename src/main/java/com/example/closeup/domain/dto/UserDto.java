7fccefcd82094204b1d7c7ef9637db6771fa8df0
98Woonho <9woonho8@gmail.com>
2024-07-16 12:17:21 +0900

    설계 파일-erd 수정

--- src/main/java/com/example/closeup/domain/dto/UserDto.java
+++ src/main/java/com/example/closeup/domain/dto/UserDto.java
@@ -1,16 +1,19 @@
 package com.example.closeup.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private String id;
     private String password;
-    private String phone;
-    private String role;
     private String name;
+    private String phone;
     private Boolean isSuspended;
-    private Boolean isAuth;
+    private String role;
 }

92e29428bfc275ace775c702c8ec52763de4b810
98Woonho <9woonho8@gmail.com>
2024-07-20 15:30:58 +0900

    .

--- src/main/java/com/example/closeup/domain/mapper/ExpertMapper.java
+++ src/main/java/com/example/closeup/domain/mapper/ExpertMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface ExpertMapper {
     List<ExpertDto> selectExpertInformation();
 
-
+//    TODO
     ExpertDto selectExpertByNickname(String nickname);
 
     ExpertDto selectExpertByUserId(String id);

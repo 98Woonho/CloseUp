00d4fdf9ed59ebcbfafe3e2594871d3fae2624a4
swLsh <113751380+swLsh@users.noreply.github.com>
2024-07-24 23:22:04 +0900

    커밋

---
+++ src/main/java/com/example/closeup/controller/Main.java
@@ -0,0 +1,12 @@
+package com.example.closeup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class Main {
+    @GetMapping("/main")
+    public void get_main(){
+
+    }
+}
+
